1b3818dbd58bcbf14dd48431381eba4eacf71f23
ionakathryn <ionakathryn123@googlemail.com>
2018-04-12 16:45:06 +0100

    further logic - some language problems in safari!

--- server.js
+++ server.js
@@ -5,8 +5,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-app.use(bodyParser.json());
-var jsonParser = bodyParser.json({ type: 'application/json'});
+// app.use(bodyParser.json());
+// var jsonParser = bodyParser.json({ type: 'application/json'});
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -19,12 +19,21 @@ app.get("/whoami", (req, res) => {
   var ipAddress = req.socket.remoteAddress;
   var ip = ipAddress.substring(ipAddress.indexOf("1"));
   var semiCol = req.get('Accept-language').indexOf(';');
+  
+  var startOS = req.get('user-agent').indexOf('(');
+  var stopOS = req.get('user-agent').indexOf(')');
+  
+  var strSplit = req.get('user-agent').split(' ');
+  var strLen = strSplit.length - 1;
+  var browser = strSplit[strLen];
+  
+  
   var resObj = {
     ip: ip,
-    details: req.get('user-agent'),
-    language: req.get('Accept-language').substring(0,semiCol)
+    operatingsystem: req.get('user-agent').substring(startOS+1,stopOS),
+    language: req.get('Accept-language').substring(0,semiCol),
+    browser: browser
   };
-
   res.json(resObj);
 })
 
